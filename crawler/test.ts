cc216e17ae126bcfd5bcb00932facc2ac7f7cce6
FrostxBitex <146770792+FrostxBitex@users.noreply.github.com>
2024-05-09 20:28:12 -0400

    Delete crawler directory

--- crawler/test.ts
+++
@@ -1,9 +0,0 @@
-import { processMangabuddyChapter } from "./scrapers/mangabuddy";
-
-async function beginTest() {
-    console.log("testing", );
-    await processMangabuddyChapter("https://mangabuddy.com/unordinary/chapter-327");
-    console.log("FINISHED");
-}
-
-beginTest();
